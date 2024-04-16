1b1a2692d4a675e40a1cbb5751964e2c9de32f11
João Portelinha <joaoportelinha@hotmail.com>
2024-04-16 12:27:59 +0100

    Fix

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ZXJEJAO3.mjs";import"./chunk-56YJGPBO.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-DLVRKORN.mjs";import"./chunk-NZ66S26X.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
